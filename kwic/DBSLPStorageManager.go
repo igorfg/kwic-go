d0ae034b1e9ff2e6a0004ee3fb588a5c9a59ab69
igorfg <igorfgds@gmail.com>
2018-05-14 09:06:42 -0300

    DBLPStorageManager is workin

--- kwic/DBSLPStorageManager.go
+++ kwic/DBSLPStorageManager.go
@@ -1,32 +1,101 @@
 package kwic
 
-// import "fmt"
-//     import "os"
-// import "log"
-// import "net/http"
-// import "encoding/json"
+import "fmt"
+import "log"
+import "net/http"
+import "encoding/json"
+import "time"
 
-// type DBLPStorageManager struct {
-// 	lines []string
-// }
+type DBLPRecord struct {
+	Result struct {
+		Query  string `json:"query"`
+		Status struct {
+			Code string `json:"@code"`
+			Text string `json:"text"`
+		} `json:"status"`
+		Time struct {
+			Unit string `json:"@unit"`
+			Text string `json:"text"`
+		} `json:"time"`
+		Completions struct {
+			Total    string `json:"@total"`
+			Computed string `json:"@computed"`
+			Sent     string `json:"@sent"`
+			C        []struct {
+				Sc   string `json:"@sc"`
+				Dc   string `json:"@dc"`
+				Oc   string `json:"@oc"`
+				ID   string `json:"@id"`
+				Text string `json:"text"`
+			} `json:"c"`
+		} `json:"completions"`
+		Hits struct {
+			Total    string `json:"@total"`
+			Computed string `json:"@computed"`
+			Sent     string `json:"@sent"`
+			First    string `json:"@first"`
+			Hit      []struct {
+				Score string `json:"@score"`
+				ID    string `json:"@id"`
+				Info  struct {
+					Authors struct {
+						Author []string `json:"author"`
+					} `json:"authors"`
+					Title  string `json:"title"`
+					Venue  string `json:"venue"`
+					Volume string `json:"volume"`
+					Number string `json:"number"`
+					Pages  string `json:"pages"`
+					Year   string `json:"year"`
+					Type   string `json:"type"`
+					Key    string `json:"key"`
+					Doi    string `json:"doi"`
+					Ee     string `json:"ee"`
+					URL    string `json:"url"`
+				} `json:"info"`
+				URL string `json:"url"`
+			} `json:"hit"`
+		} `json:"hits"`
+	} `json:"result"`
+}
 
-// func (this *DBLPStorageManager) Init() {
-// 	var query string
+type DBLPStorageManager struct {
+	lines []string
+}
 
-// 	fmt.Print("Enter the DBLP search criteria (such as the author name): ")
-// 	fmt.Scan(&query)
-// 	url := "http://dblp.org/search/publ/api?q=" + query + "&format=json"
-// 	lines := makeRequest(url)
-// }
+func (this *DBLPStorageManager) Init() {
+	var query string
 
-// func makeRequest(url string) []string {
-// 	client := &http.Client{Timeout: 10 * time.Second}
-// 	request, err := client.Get(url)
+	fmt.Print("Enter the DBLP search criteria (such as the author name): ")
+	fmt.Scan(&query)
+	url := "http://dblp.org/search/publ/api?q=" + query + "&format=json"
+	record := new(DBLPRecord)
+	this.lines = makeRequest(url, record)
+}
 
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer request.Body.Close()
+func (this *DBLPStorageManager) Line(index int) string {
+	return this.lines[index]
+}
 
-// 	result := json.NewDecoder(request.Body).Decode(target)
-// }
+func (this *DBLPStorageManager) Length() int {
+	return len(this.lines)
+}
+
+func makeRequest(url string, record *DBLPRecord) []string {
+	client := &http.Client{Timeout: 10 * time.Second}
+	request, err := client.Get(url)
+	var titles []string
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer request.Body.Close()
+
+	json.NewDecoder(request.Body).Decode(record)
+
+	for _, hit := range record.Result.Hits.Hit {
+		titles = append(titles, hit.Info.Title)
+	}
+
+	return titles
+}
--- main.go
+++ main.go
@@ -1,82 +1,61 @@
 package main
 
-import "time"
-
-//import "strings"
-//import "github.com/igorfg/kwic-go/kwic"
-
-import "net/http"
-import "encoding/json"
-
-var myClient = &http.Client{Timeout: 10 * time.Second}
-
-func getJson(url string, target interface{}) error {
-	r, err := myClient.Get(url)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
-type DBLP struct {
-	Result struct {
-		Query  string `json:"query"`
-		Status struct {
-			Code string `json:"@code"`
-			Text string `json:"text"`
-		} `json:"status"`
-		Time struct {
-			Unit string `json:"@unit"`
-			Text string `json:"text"`
-		} `json:"time"`
-		Completions struct {
-			Total    string `json:"@total"`
-			Computed string `json:"@computed"`
-			Sent     string `json:"@sent"`
-			C        []struct {
-				Sc   string `json:"@sc"`
-				Dc   string `json:"@dc"`
-				Oc   string `json:"@oc"`
-				ID   string `json:"@id"`
-				Text string `json:"text"`
-			} `json:"c"`
-		} `json:"completions"`
-		Hits struct {
-			Total    string `json:"@total"`
-			Computed string `json:"@computed"`
-			Sent     string `json:"@sent"`
-			First    string `json:"@first"`
-			Hit      []struct {
-				Score string `json:"@score"`
-				ID    string `json:"@id"`
-				Info  struct {
-					Authors struct {
-						Author []string `json:"author"`
-					} `json:"authors"`
-					Title  string `json:"title"`
-					Venue  string `json:"venue"`
-					Volume string `json:"volume"`
-					Number string `json:"number"`
-					Pages  string `json:"pages"`
-					Year   string `json:"year"`
-					Type   string `json:"type"`
-					Key    string `json:"key"`
-					Doi    string `json:"doi"`
-					Ee     string `json:"ee"`
-					URL    string `json:"url"`
-				} `json:"info"`
-				URL string `json:"url"`
-			} `json:"hit"`
-		} `json:"hits"`
-	} `json:"result"`
-}
+import "fmt"
+import "strings"
+import "github.com/igorfg/kwic-go/kwic"
 
 func main() {
-	foo1 := new(DBLP) // or &Foo{}
-	getJson("http://dblp.org/search/publ/api?q=golang&format=json", &foo1)
-	for i := 0; i < len(foo1.Result.Hits.Hit); i++ {
-		println(foo1.Result.Hits.Hit[i].Info.Title)
+	// var fsm kwic.DataStorageManager = &kwic.FileBasedStorageManager{}
+	// var im kwic.IndexManager = kwic.IndexManager{}
+
+	// fsm.Init()
+	// im.Init()
+
+	// for lineNumber := 0; lineNumber < fsm.Length(); lineNumber++ {
+	// 	line := fsm.Line(lineNumber)
+	// 	words := strings.Split(line, " ")
+
+	// 	for pos := 0; pos < len(words); pos++ {
+	// 		im.Hash(words[pos], line, pos)
+	// 	}
+	// }
+
+	// sortedWords := im.SortedWords()
+	// wordShift := kwic.WordShift{}
+
+	// for _, w := range sortedWords {
+	// 	for _, tuple := range im.OccurencesOfWord(w) {
+	// 		func(line string, pos int) {
+	// 			fmt.Println(strings.Join(wordShift.Shift(strings.Split(line, " "), pos, 0), " "))
+	// 		}(tuple.First.(string), tuple.Second.(int))
+	// 	}
+	// }
+
+	var dblpsm kwic.DataStorageManager = &kwic.DBLPStorageManager{}
+	var im kwic.IndexManager = kwic.IndexManager{}
+
+	dblpsm.Init()
+	im.Init()
+
+	for lineNumber := 0; lineNumber < dblpsm.Length(); lineNumber++ {
+		line := dblpsm.Line(lineNumber)
+		words := strings.Split(line, " ")
+
+		for pos := 0; pos < len(words); pos++ {
+			im.Hash(words[pos], line, pos)
+		}
 	}
 
+	sortedWords := im.SortedWords()
+	wordShift := kwic.WordShift{}
+
+	for _, w := range sortedWords {
+		for _, tuple := range im.OccurencesOfWord(w) {
+			func(line string, pos int) {
+				fmt.Println(strings.Join(wordShift.Shift(strings.Split(line, " "), pos, 0), " "))
+			}(tuple.First.(string), tuple.Second.(int))
+		}
+	}
+
+	fmt.Println("Fim da Execução")
 }
